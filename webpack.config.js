50a408cfd46a30be69539e4137f3b4c685a7d0f9
arno-bagbila <beat4free@gmail.com>
2015-12-01 15:40:11 +0000

    Add webpack configuration with TARGET and var common to use npm_lifecycle_event

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,16 @@ var path = require("path");
 var HtmlwebpackPlugin = require("html-webpack-plugin");
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 var webpack = require("webpack");
+var merge = require("webpack-merge");
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, "app"),
   build: path.join(__dirname, "build"),
 };
 
-module.exports = {
+var common = {
   entry:PATHS.app,
-  output: {
-    path:PATHS.build,
-    filename : "bundle.js"
-  },
   module:{
     loaders:[
       {
@@ -23,24 +21,32 @@ module.exports = {
       }
     ]
   },
-  devServer:{
-    historyApiFallback:true,
-    hot:true,
-    inline:true,
-    progress:true,
-
-    //display only errors to reduce the amount output
-    stats:"errors-only",
-
-    //parse host and port from env so this is easy to customize
-    host:process.env.HOST,
-    port:process.env.PORT
-  },
   plugins:[
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: "Zi Blog App"
     }),
     new OpenBrowserPlugin({ url: 'http://localhost:8080' })
   ]
 };
+
+if(TARGET === "start" || !TARGET){
+  module.exports = merge(common, {
+    devtool:"eval-source-map",
+    devServer:{
+      historyApiFallback:true,
+      hot:true,
+      inline:true,
+      progress:true,
+
+      //display only errors to reduce the amount output
+      stats:"errors-only",
+
+      //parse host and port from env so this is easy to customize
+      host:process.env.HOST,
+      port:process.env.PORT
+    },
+    plugins:[
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+  })
+}
